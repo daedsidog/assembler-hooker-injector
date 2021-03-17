b3e87c3db3f3a699ce69a3193b8c4b102cee8700
J <41439659+daeds1dog@users.noreply.github.com>
2021-03-17 05:11:12 +0200

    Complete hooking funcs

--- ahi.cpp
+++ ahi.cpp
@@ -3,25 +3,19 @@
 #include <iostream>
 
 AHI::AHI(LPVOID image_base){
-    this->base_addr = GetModuleHandle(0x0);
-    this->pe = image_base;
+    base_addr = GetModuleHandle(0x0);
+    pe = image_base;
 }
 
 AHI::~AHI(){
-    for(auto const& func_backup : func_backups){
-        delete[] func_backup.second;
+    for(const auto& func_backup : func_backups){
+        unhook_func(func_backup.first);
     }
 }
 
-LPVOID AHI::hook_dll_func(std::string dll, std::string func_name, LPVOID dst_func){
-    HMODULE module_handle = GetModuleHandle(dll.c_str());
-    if (!module_handle){
-        std::cerr << __FUNCTION__ << ": GetModuleHandle error: " << GetLastError() << std::endl;
-        return 0;
-    }
-    LPVOID relative_func_addr = (LPVOID)GetProcAddress(module_handle, func_name.c_str());
-    if(!relative_func_addr){
-        std::cerr << __FUNCTION__ << ": GetProcAddress error: " << GetLastError() << std::endl;
+LPVOID AHI::hook_func(LPVOID func_addr, LPVOID dst_func){
+    if(func_backups.find(func_addr) != func_backups.end()){
+        std::cerr << __FUNCTION__ << ": " << func_addr << " is already hooked!" << std::endl;
         return 0;
     }
     HANDLE process_handle = GetCurrentProcess();
@@ -29,23 +23,21 @@ LPVOID AHI::hook_dll_func(std::string dll, std::string func_name, LPVOID dst_fun
         std::cerr << __FUNCTION__ << ": GetCurrentProcess error: " << GetLastError() << std::endl;
         return 0;
     }
-
-    // sizeof(LPVOID) + 2 allows this to be portable across different architectures.
-    if(!ReadProcessMemory(process_handle, relative_func_addr, func_backups[relative_func_addr], JMP_OPCODE_SIZE, 0)){
+    if(!ReadProcessMemory(process_handle, func_addr, func_backups[func_addr], JMP_OPCODE_SIZE, 0)){
         std::cerr << __FUNCTION__ << ": ReadProcessMemory error: " << GetLastError() << std::endl;
         return 0;
     }
-    LPVOID func_addr = (LPVOID)((uintptr_t)dst_func - (uintptr_t)relative_func_addr - (JMP_OPCODE_SIZE - 1));
+    LPVOID dst_func_addr = (LPVOID)((uintptr_t)dst_func - (uintptr_t)func_addr - (JMP_OPCODE_SIZE - 1));
     DWORD previous_protection;
-    VirtualProtect(relative_func_addr, JMP_OPCODE_SIZE, PAGE_EXECUTE_READWRITE,
+    VirtualProtect(func_addr, JMP_OPCODE_SIZE, PAGE_EXECUTE_READWRITE,
                    &previous_protection);
-    memcpy(&jmp_opcode[1], &func_addr, ADDR_SIZE);
-    if(!WriteProcessMemory(process_handle, (LPVOID)relative_func_addr, jmp_opcode, JMP_OPCODE_SIZE,
+    memcpy(&jmp_opcode[1], &dst_func_addr, ADDR_SIZE);
+    if(!WriteProcessMemory(process_handle, (LPVOID)func_addr, jmp_opcode, JMP_OPCODE_SIZE,
                        0)){
         std::cerr << __FUNCTION__ << ": WriteProcessMemory error: " << GetLastError() << std::endl;
         return 0;
     }
-    if(!VirtualProtect((LPVOID)relative_func_addr, JMP_OPCODE_SIZE, previous_protection,
+    if(!VirtualProtect((LPVOID)func_addr, JMP_OPCODE_SIZE, previous_protection,
                    &previous_protection)){
         std::cerr << __FUNCTION__ << ": VirtualProtect error: " << GetLastError() << std::endl;
         return 0;
@@ -54,5 +46,59 @@ LPVOID AHI::hook_dll_func(std::string dll, std::string func_name, LPVOID dst_fun
         std::cerr << __FUNCTION__ << ": FlushInstructionCache error: " << GetLastError() << std::endl;
         return 0;
     }
-    return relative_func_addr;
+    std::cout << "Hooked " << dst_func_addr << " to " << func_addr << std::endl;
+    return func_addr;
+}
+
+LPVOID AHI::unhook_func(LPVOID func_addr){
+    if(func_backups.find(func_addr) == func_backups.end()){
+        std::cerr << __FUNCTION__ << ": " << func_addr << " is not hooked!" << std::endl;
+        return 0;
+    }
+    HANDLE process_handle = GetCurrentProcess();
+    if(!process_handle){
+        std::cerr << __FUNCTION__ << ": GetCurrentProcess error: " << GetLastError() << std::endl;
+        return 0;
+    }
+    if (!WriteProcessMemory(process_handle, (LPVOID)func_addr, func_backups[func_addr], JMP_OPCODE_SIZE, 0)){
+        std::cerr << __FUNCTION__ << ": WriteProcessMemory error: " << GetLastError() << std::endl;
+        return 0;
+    }
+    if(!FlushInstructionCache(process_handle, 0, 0)){
+        std::cerr << __FUNCTION__ << ": FlushInstructionCache error: " << GetLastError() << std::endl;
+        return 0;
+    }
+    std::cout << "Unooked function from " << func_addr << std::endl;
+    func_backups.erase(func_addr);
+    return func_addr;
+}
+
+LPVOID AHI::hook_dll_func(std::string dll, std::string func_name, LPVOID dst_func_addr){
+    HMODULE module_handle = GetModuleHandle(dll.c_str());
+    if (!module_handle){
+        std::cerr << __FUNCTION__ << ": GetModuleHandle error: " << GetLastError() << std::endl;
+        return 0;
+    }
+    LPVOID func_addr = (LPVOID)GetProcAddress(module_handle, func_name.c_str());
+    if(!func_addr){
+        std::cerr << __FUNCTION__ << ": GetProcAddress error: " << GetLastError() << std::endl;
+        return 0;
+    }
+    std::cout << dll << "." << func_name << ": ";
+    return func_addr = hook_func(func_addr, dst_func_addr);
+}
+
+LPVOID AHI::unhook_dll_func(std::string dll, std::string func_name){
+    HMODULE module_handle = GetModuleHandle(dll.c_str());
+    if (!module_handle){
+        std::cerr << __FUNCTION__ << ": GetModuleHandle error: " << GetLastError() << std::endl;
+        return 0;
+    }
+    LPVOID func_addr = (LPVOID)GetProcAddress(module_handle, func_name.c_str());
+    if(!func_addr){
+        std::cerr << __FUNCTION__ << ": GetProcAddress error: " << GetLastError() << std::endl;
+        return 0;
+    }
+    std::cout << dll << "." << func_name << ": ";
+    return unhook_func(func_addr);
 }
--- ahi.hpp
+++ ahi.hpp
@@ -6,23 +6,23 @@
 #include <map>
 
 #define ADDR_SIZE 4
-#define JMP_OPCODE 0xe9, 0x0, 0x0, 0x0, 0x0, 0xc3
+#define JMP_OPCODE_BYTES 0xe9, 0x0, 0x0, 0x0, 0x0, 0xc3
 #define JMP_OPCODE_SIZE 6
 
 class AHI {
     LPVOID base_addr = 0x0;
     LPVOID pe = 0x0;
-    BYTE jmp_opcode[JMP_OPCODE_SIZE] = { JMP_OPCODE };
+    BYTE jmp_opcode[JMP_OPCODE_SIZE] = { JMP_OPCODE_BYTES };
 
-    std::map<LPVOID, BYTE*> func_backups;
+    std::map<LPVOID, BYTE[JMP_OPCODE_SIZE]> func_backups;
 public:
     AHI(LPVOID image_base = 0x0);
     ~AHI();
 
-    LPVOID hook_dll_func(std::string dll, std::string func_name, LPVOID dst_func);
+    LPVOID hook_func(LPVOID func_addr, LPVOID dst_func_addr);
+    LPVOID unhook_func(LPVOID func_addr);
+    LPVOID hook_dll_func(std::string dll, std::string func_name, LPVOID dst_func_addr);
     LPVOID unhook_dll_func(std::string dll, std::string func_name);
-    LPVOID hook_func(LPVOID func, LPVOID dst_func);
-    LPVOID unhook_func(LPVOID func);
 };
 
 #endif
